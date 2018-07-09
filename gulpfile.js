d6e0e9134d6faee392c540cb87d7834fe9feb2e3
avijit sarkar <avisofficeone@gmail.com>
2018-07-10 02:47:31 +0530

    sucess : gulp is compiling pug ->mjml->html

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,73 @@ var
     _if = require('gulp-if')
     //pugs
     pug = require('gulp-pug'),
-    html2jade = require('gulp-html2jade')
+    html2jade = require('gulp-html2jade'),
+    //mjml
+    mjml = require("gulp-mjml")
 ;
 
+/**
+ * @type Move img src files
+ * @desc : pipe
+ * @version 1.0
+ */
+gulp.task('sft:img', gulp.parallel((done) => {
+    pump([
+        gulp.src(vl.src.img),
+        imgMin(),
+        gulp.dest(vl.dist.img)
+    ], done());
+}));
+/**
+ * @type cleaning 
+ * @desc none 
+ * @version 1.0.0
+ */
+gulp.task('clr:bin', gulp.parallel((done) => {
+    del(vl.cmommon.bin + '/*');
+    done();
+}));
+gulp.task('clr:dist', gulp.parallel((done) => {
+    del(vl.cmommon.dist + '/*');
+    done();
+}));
+
+/**
+ * @type Live Reload
+ * @desc : browser-sync | none 
+ * @version 1.0
+ */
+gulp.task('show', gulp.parallel((done) => {
+    browserSync.init({
+        server: dirs.dist
+    });
+    done();
+}));
+
+
+/**
+ * @type compile pug -> html
+ */
+
+gulp.task('pug:min', gulp.parallel((done) => {
+    pump([
+        gulp.src(vl.pug.entryfile),
+        plumber(),
+        pug({
+            pretty: true
+        }),
+        // mjml(),
+        gulp.dest(vl.pug.dist)
+    ], done());
+}))
+gulp.task('pug:full', gulp.parallel((done) => {
+    pump([
+        gulp.src(vl.pug.entryfile),
+        plumber(),
+        pug({
+            pretty: true
+        }),
+        mjml(),
+        gulp.dest(vl.pug.dist)
+    ], done());
+}));
